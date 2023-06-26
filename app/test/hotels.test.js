97f88a59ed0074745c9d3c3e435892ec942218c4
frsnmk <razedman@gmail.com>
2023-06-26 10:27:14 +0700

    Fix some error on testing

--- app/test/hotels.test.js
+++ app/test/hotels.test.js
@@ -16,11 +16,11 @@ describe("Service Hotels Test", () => {
   test("Fetch should return message data is empty", async () => {
     Hotel.find.mockResolvedValue([]);
     const data = await hotelSvc.fetch();
-    expect(data).toEqual({ message: "Data is empty" });
+    expect(data).toEqual({message: "Data is empty"});
   });
 
   test("Create should create a new hotel and return it", async () => {
-    const mockCreatedHotel = { _id: hotelId, ...hotelsMock[0] };
+    const mockCreatedHotel = {_id: hotelId, ...hotelsMock[0]};
     Hotel.prototype.save.mockResolvedValue(mockCreatedHotel);
     const createdHotel = await hotelSvc.create(hotelsMock[0]);
 
@@ -29,21 +29,20 @@ describe("Service Hotels Test", () => {
   });
 
   test("GetOne should retrieve a hotel by id", async () => {
-    const mockHotelData = { _id: hotelId, ...hotelsMock[0] };
+    const mockHotelData = {_id: hotelId, ...hotelsMock[0]};
     Hotel.findOne.mockResolvedValueOnce(mockHotelData);
 
     const hotel = await hotelSvc.getOne(hotelId);
-    expect(Hotel.findOne).toHaveBeenCalledWith({ _id: hotelId });
+    expect(Hotel.findOne).toHaveBeenCalledWith({_id: hotelId});
     expect(hotel).toEqual(mockHotelData);
   });
 
   test("Update should update a hotel by ID and return the updated data", async () => {
     const mockUpdateData = {
-      _id: hotelId,
       name: "Oyo Hotel",
       address: "Cianjur",
     };
-    const mockUpdatedData = { _id: hotelId, ...hotelsMock[0] };
+    const mockUpdatedData = {_id: hotelId, ...mockUpdateData};
     Hotel.findOneAndUpdate.mockResolvedValueOnce(mockUpdatedData);
 
     const updatedHotel = await hotelSvc.update(hotelId, mockUpdateData);
@@ -52,20 +51,20 @@ describe("Service Hotels Test", () => {
       {
         _id: hotelId,
       },
-      { ...mockUpdateData },
-      { replace: true }
+      {...mockUpdateData},
+      {replace: true}
     );
 
     expect(updatedHotel).toEqual(mockUpdatedData);
   });
 
   test("Destroy should delete a hotel by id and return deleted data", async () => {
-    const mockDeletedData = { _id: hotelId, ...hotelsMock[0] };
+    const mockDeletedData = {_id: hotelId, ...hotelsMock[0]};
     Hotel.findOneAndDelete.mockResolvedValueOnce(mockDeletedData);
 
     const deletedHotel = await hotelSvc.destroy(hotelId);
 
-    expect(Hotel.findOneAndDelete).toHaveBeenCalledWith({ _id: hotelId });
+    expect(Hotel.findOneAndDelete).toHaveBeenCalledWith({_id: hotelId});
     expect(deletedHotel).toEqual(mockDeletedData);
   });
 });
