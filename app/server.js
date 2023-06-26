b2692321b856debe0d5df452aeb6cffccd6ac5f6
frsnmk <razedman@gmail.com>
2023-06-26 15:01:31 +0700

    Change port base from ENV

--- app/server.js
+++ app/server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
 const dayjs = require("dayjs");
-      const runningOn = dayjs().format('YYYY-MM-DD HH:mm:ss');
+const runningOn = dayjs().format("YYYY-MM-DD HH:mm:ss");
 
-app.listen(3000, () => {
-  console.log("application listen on http://localhost:3000 ", runningOn);
+app.listen(process.env.PORT, () => {
+  console.log(
+    "application listen on http://localhost:".process.env.PORT,
+    runningOn
+  );
 });
